3f97af77f3ddccdc9e414ee40dcc3ad0495094bb
cyclic-goose <bstevens2225@gmail.com>
2026-03-07 14:50:34 -0800

    flip field centric

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -92,8 +92,8 @@ public class DriveCommands {
                   linearVelocity.getY() * drive.getMaxLinearSpeedMetersPerSec(),
                   omega * drive.getMaxAngularSpeedRadPerSec());
           boolean isFlipped =
-              !DriverStation.getAlliance().isPresent()
-                  && DriverStation.getAlliance().get() == Alliance.Red;
+              !(DriverStation.getAlliance().isPresent()
+                  && DriverStation.getAlliance().get() == Alliance.Red);
           drive.runVelocity(
               ChassisSpeeds.fromFieldRelativeSpeeds(
                   speeds,
